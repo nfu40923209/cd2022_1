89e460529d486bb739a8964351ce5a833e487232
Lu8658813 <40923209@gm.nfu.edu.tw>
2022-03-17 15:22:50 +0800

    add hwpage and W3

---
+++ downloads/21-1.js
@@ -0,0 +1,8 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 60,
+  buttonFrameLength: 30,
+  buttonFrameOffset: 15,
+  frameStops: {
+  },
+};
---
+++ downloads/21-2.js
@@ -0,0 +1,8 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 30,
+  buttonFrameLength: 15,
+  buttonFrameOffset: 7,
+  frameStops: {
+  },
+};
